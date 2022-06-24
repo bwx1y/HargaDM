106130938922160d072d4c6bf284bccf2a430203
bwx1y <nedynugroho2007@gmail.com>
2022-06-23 20:06:34 -0700

    update V2

--- src/js/main.js
+++ src/js/main.js
@@ -4,21 +4,20 @@ const loding = document.querySelector("#loding");
 
 $.getJSON("/src/data/main.json", (data) => {
   $.each(data.MobileLegend, (i, data) => {
-    console.log(data);
+    
     $("#mobileLegends").append("<tr><td>" + data.Diamond + " 💎</td><td>" + data.Harga + "</td></tr>");
   });
 });
 $.getJSON("/src/data/main.json", (data) => {
   $.each(data.FreeFire, (i, data) => {
-    console.log(data);
+    
     $("#freeFire").append("<tr><td>" + data.Diamond + " 💎</td><td>" + data.Harga + "</td></tr>");
   });
 });
 $.getJSON("/src/data/main.json", (data) => {
   $.each(data.Member, (i, data) => {
-    console.log(data);
     $("#member").append(
-      '<div class="col-md-4"><div class="card border-dark mb-3"><h5 class="card-header">' + data.Data + '</h5><div class="card-body"><h5 class="card-title">' + data.Member + "💳</h5><p>" + data.Harga + "</p></div></div></div>"
+      '<div class="col-md-4"><div class="card border-dark mb-3" width><h5 class="card-header">' + data.Data + '</h5><div class="card-body"><h5 class="card-title">' + data.Member + "💳</h5><p>" + data.Harga + "</p></div></div></div>"
     );
   });
 });
@@ -28,11 +27,11 @@ $("#btnDwn").click((e) => {
   $("#btnDwn").toggleClass("d-none");
   $("#btnUp").toggleClass("d-none");
   setTimeout(() => {
-    $("#loding").remove();
+    $("#loding").toggleClass("d-none");
     $("#list").toggleClass("d-none");
     $("#member").toggleClass("d-none");
   }, 3000);
-  $("#loding").append('<div class="col-12 spinner-border"><span class="visually-hidden"></span></div>');
+  $("#loding").toggleClass("d-none");
 });
 $("#btnUp").click((e) => {
   e.preventDefault();
